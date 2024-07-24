ee2536764b336bd5a0e46070abaed2a0032962f7
CttCJim <31054746+CttCJim@users.noreply.github.com>
2024-07-24 00:02:49 -0600

    Update xvid_adjustments.js

--- xvid_adjustments.js
+++ xvid_adjustments.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         xvid adjustments
 // @namespace    http://tampermonkey.net/
-// @version      2024.04.19.02
+// @version      2024.07.24.01
 // @description  Remove Red and ads from xvideos and make QoL changes
 // @author       CttCJim
 // @match        https://www.xvideos.com/*
